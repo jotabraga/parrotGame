ac05f9042658d198abf806b859e4b1fa9548ff93
jotabraga <jpmbraga.feg@gmail.com>
2021-04-08 00:22:15 -0300

    Implementação da distribuição aleatoria de cartas

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-let cardsNumber;                                                        //declarando o numero de cartas
-let rounds;
-let gamecard = [];
+let cardsNumber;        //declarando o numero de cartas
+let rounds=0;       //variavel para guardar o numero de jogadas
+let gamecard = [];      //array da div das cartas
 const cardPictures = ["charlie","linus","lucy","paty","sally","snoopy","woodstock","back"];
 
 function gameInit(){
     const cardsNumber = parseInt(prompt("Com quantas cartas deseja jogar?"));
-    if (cardsNumber%2 !== 0 || cardsNumber < 4 || cardsNumber > 14){     //se num de cartas for impar ou > que 14 ou < que 4 
-        gameInit();                                                     //a função se chama repetindo a pergunta, dá pra usar while também 
+    if (cardsNumber%2 !== 0 || cardsNumber < 4 || cardsNumber > 14){        //se num de cartas for impar ou > que 14 ou < que 4 
+        gameInit();                                                         //a função se chama repetindo a pergunta, dá pra usar while também 
     }else{
-        gameStart(cardsNumber);
+        gameStart(cardsNumber);                                             //se tudo ok começa o jogo
     }    
 }
 
@@ -18,23 +18,32 @@ function gameStart(cardsNumber){                                        //inicio
     game.innerHTML = '';
 
     for(let i=0, j=0; i < cardsNumber; i++){              
-            if(i%2 === 0 && i !== 0){
+        if(i%2 === 0 && i !== 0){
             j++;                
-            }                  
-            game.innerHTML += `<div class="${gamecard[i]}"><div class="front-card"><img src="Midia/${cardPictures[j]}.png"></div><div class="back-card" onclick="turncard(this)"><img src="Midia/${cardPictures[7]}.png"></div></div>`
+        }   
+        console.log(gamecard[i]);               
+        game.innerHTML += `<div class="card${i}"><div class="front-card"><img src="Midia/${cardPictures[j]}.png"></div><div class="back-card" onclick="turnCard(this)"><img src="Midia/${cardPictures[7]}.png"></div></div>`
     }
-    const gamesorted = document.querySelectorAll('.gamecard');
-    gamesorted.sort(comparator);
+    for(let k=0; k < cardsNumber; k++){
+        gamecard[k] = document.querySelector(`.card${k}`);
     }
+    gamecard.sort(comparator);
+    }
+
+    
 
 function comparator(){
     return Math.random() - 0.5;
 }
-    
 
-
-function turnCard(){                                                    //função para virar a carta e ver se a proxima é igual ou diferente
-
+function turnCard(element){    
+    const parent = element.parentNode;
+    const brother = parent.firstChild;
+    console.log(parent);
 }
+  //  this.classList.add('hidden');
+   // rounds++;                                               //função para virar a carta e ver se a proxima é igual ou diferente
+
+
 
 gameInit();
